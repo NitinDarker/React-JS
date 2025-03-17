77fed1d2dd50cc23b6b8027cb1047673d08b3149
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-17 17:48:22 +0530

    Improved clock with color changing features

--- React.dev/04-Clock/src/App.jsx
+++ React.dev/04-Clock/src/App.jsx
@@ -7,11 +7,27 @@ export default function App() {
     setInterval(() => {
         setTime(new Date().toLocaleTimeString())
     }, 1000);
-    
-    let color = "blue";
+
+    let [color, setColor] = useState('white');
+    const handleChange = (event) => {
+        setColor(event.target.value);
+    };
+
     return (
         <>
+            <label htmlFor="colors">Pick a color: </label>
+            <select id="colors" value='white' onChange={handleChange}>
+                <option value="white">White</option>
+                <option value="red">Red</option>
+                <option value="blue">Blue</option>
+                <option value="green">Green</option>
+                <option value="yellow">Yellow</option>
+                <option value="pink">Pink</option>
+            </select>
             <Clock color={color} time={time}/>
+            <p></p>
         </>
     )
+
+    
 }
