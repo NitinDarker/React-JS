1f30ed4f4dc57ebff548b09f68435b7c8b96822c
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-23 23:36:38 +0530

    Fixed the feedback app

--- experiments/src/App.jsx
+++ experiments/src/App.jsx
@@ -1,31 +1,10 @@
-import { useState, memo } from "react";
+import FeedbackForm from './components/Feedback.jsx'
+import Greet from './components/Greet.jsx'
 
-function App() {
-  return (
-    <>
-      <HeaderWithButton></HeaderWithButton>
-      <Header title={"I am a static component"}></Header>
-    </>
-  );
+export default function App() {
+    return (
+        <>
+            <FeedbackForm />
+        </>
+    )
 }
-
-function HeaderWithButton() {
-  const [myTitle, changeTitle] = useState("Nitin");
-
-  function titleFun() {
-    changeTitle(Math.random());
-  }
-
-  return (
-    <>
-      <button onClick={titleFun}>Click me to change title</button>
-      <Header title={"Title of this page is: " + myTitle}>Title of this page is:</Header>
-    </>
-  );
-}
-
-function Header({ title }) {
-  return <div>{title}</div>;
-}
-
-export default App;
