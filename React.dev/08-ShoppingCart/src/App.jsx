77cddb1554f4d0433eeb9d86a71af1f9de487f2b
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-27 19:04:10 +0530

    Shopping Cart

---
+++ React.dev/08-ShoppingCart/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+  
+  function handleDecreaseClick(productId) {
+    let newArr = products.map((p) => {
+        if (p.id === productId) {
+            return {
+                ...p, 
+                count: p.count - 1
+            }
+        } else {
+            return p;
+        }
+    });
+    newArr = newArr.filter((p) => {
+        return p.count > 0;
+    })
+    setProducts(newArr);
+
+    // if (products[productId].count == 1) {
+    //     setProducts(products.filter((p) => {
+    //         if (p.id != productId) return true;
+    //         return false; 
+    //     }));
+    //     return;
+    // }
+    // setProducts(products.map((p)=> {
+    //     if (p.id == productId) {
+    //         return {
+    //             ...p,
+    //             count: p.count - 1
+    //         }
+            
+    //     } else {
+    //         return p;
+    //     }
+    // }));
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+          <button onClick={() => {
+            handleDecreaseClick(product.id);
+          }}>
+            –
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
