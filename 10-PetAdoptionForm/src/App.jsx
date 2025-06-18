753bc2799d49cb6a0d0c02bb1bf85a0e94faa72f
NitinDarker <nitinsharma9829547681@gmail.com>
2025-06-18 21:25:18 +0530

    pet adoption form -> building the input form component

---
+++ 10-PetAdoptionForm/src/App.jsx
@@ -0,0 +1,14 @@
+import InputForm from '../components/InputForm'
+
+function App () {
+  return (
+    <>
+      <div className='bg-black text-white h-dvh flex flex-col items-center'>
+        <h1>Pet Adoption Form</h1>
+        <InputForm /> 
+      </div>
+    </>
+  )
+}
+
+export default App
